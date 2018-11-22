28a535b565d4414830cdeb6d8194cc1da3931f07
qixin <qixin@haoqix.com>
2018-11-22 16:14:42 +0800

    config update

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # project
 	pc-bg-api
-	a project of api for pc-bg-app
+	A project of api for pc-bg-app
 
 
 # Start
-	1.git clone 
+	1.git clone https://github.com/XinQiXiao/pc-bg-api.git
 	2.npm install or yarn install
+
+# run
+	1.npm start 
+	2. In chrome type localhost:3000
 
 # branch and target
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// use post
+router.post('/', function(req, res){
+  res.send('Got a POST request')
+})
+
+// use put
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user')
+})
+
+// use delete
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user')
+})
+
 module.exports = router;
